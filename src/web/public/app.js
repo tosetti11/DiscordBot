0d4e39d47410d7743eb80fbf236869aafa2882fe
tosetti11 <tosetti11@users.noreply.github.com>
2026-02-18 12:58:49 -0500

    Add sandbox permissions to Widgetbot iframe for login popups

--- src/web/public/app.js
+++ src/web/public/app.js
@@ -926,11 +926,11 @@ function loadChatWidget() {
       const src = channelId
         ? `https://e.widgetbot.io/channels/${guildId}/${channelId}`
         : `https://e.widgetbot.io/channels/${guildId}`;
-      wrap.innerHTML = `<iframe src="${src}" allow="clipboard-write; fullscreen"></iframe>`;
+      wrap.innerHTML = `<iframe src="${src}" allow="clipboard-write; fullscreen" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-forms"></iframe>`;
       chatWidgetLoaded = true;
     })
     .catch(() => {
-      wrap.innerHTML = `<iframe src="https://e.widgetbot.io/channels/${guildId}" allow="clipboard-write; fullscreen"></iframe>`;
+      wrap.innerHTML = `<iframe src="https://e.widgetbot.io/channels/${guildId}" allow="clipboard-write; fullscreen" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-forms"></iframe>`;
       chatWidgetLoaded = true;
     });
 }
