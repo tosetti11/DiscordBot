71d0d092b4dbdfedeb84bc90c78316ae1ba42356
tosetti11 <tosetti11@users.noreply.github.com>
2026-02-17 22:58:22 -0500

    Debug avatar logging

--- src/web/server.js
+++ src/web/server.js
@@ -274,10 +274,15 @@ function createWebServer() {
     if (discordClient) {
       try {
         const dUser = await discordClient.users.fetch(user.discordId).catch(() => null);
+        console.log(`[DEBUG Avatar] discordId=${user.discordId} dUser=${!!dUser} avatar=${dUser?.avatar} url=${dUser?.displayAvatarURL({ size: 128 })}`);
         if (dUser) {
           user.avatar = dUser.displayAvatarURL({ size: 128 });
         }
-      } catch (e) {}
+      } catch (e) {
+        console.log(`[DEBUG Avatar] Error:`, e.message);
+      }
+    } else {
+      console.log(`[DEBUG Avatar] No discordClient available`);
     }
     res.json(user);
   });
