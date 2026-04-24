3b98ab319a267f8fd9c8b8eb51787f3064a61c36
tosetti11 <tosetti11@users.noreply.github.com>
2026-04-24 08:17:18 -0400

    fix: remove forced page reload from service worker activate (was causing sign-in loop)

--- src/web/public/service-worker.js
+++ src/web/public/service-worker.js
@@ -10,7 +10,5 @@ self.addEventListener('activate', event => {
     caches.keys().then(keys =>
       Promise.all(keys.map(k => caches.delete(k)))
     ).then(() => self.registration.unregister())
-     .then(() => self.clients.matchAll())
-     .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });
